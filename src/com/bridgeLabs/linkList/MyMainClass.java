42a95b5e389361fab3c4c4387e81ba6d7f520379
mukulBillore <mukulbillore88@gmail.com>
2021-12-23 18:54:04 +0530

    added: creating link list from the node

--- src/com/bridgeLabs/linkList/MyMainClass.java
+++ src/com/bridgeLabs/linkList/MyMainClass.java
@@ -3,6 +3,7 @@ package com.bridgeLabs.linkList;
 public class MyMainClass {
 
 	public static void main(String[] args) {
+		//uc1
 		//object to create the node
 		OneNode node1 = new OneNode(56);
 		OneNode node2 = new OneNode(30);
@@ -14,8 +15,16 @@ public class MyMainClass {
 		list1.addANode(node1);
 		list1.addANode(node2);
 		list1.addANode(node3);
-		print.PrintLinklist(list1);
-		
+		//print.PrintLinklist(list1);
+		//uc2
+		OneNode nodea = new OneNode(30);
+		OneNode nodeb = new OneNode(56);
+		OneNode nodec = new OneNode(70);
+		LinkListCreation<OneNode> list2 = new LinkListCreation<OneNode>();
+		list2.addANode(nodea);
+		list2.addANode(nodeb);
+		list2.addANode(nodec);
+		print.PrintLinklist(list2);
 	}
 
 }
