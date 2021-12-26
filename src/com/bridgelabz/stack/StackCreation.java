821690d64f309270fda7ff94114d9d68577f1ed9
mukulBillore <mukulbillore88@gmail.com>
2021-12-26 23:20:05 +0530

    added the functionality to push and print stack

---
+++ src/com/bridgelabz/stack/StackCreation.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.stack;
+
+import com.bridgeLabs.linkList.LinkListCreation;
+import com.bridgeLabs.linkList.OneNode;
+
+public class StackCreation<N> {
+LinkListCreation<N> stack; 
+
+public void push(OneNode newNode) {
+	stack.addANode(newNode);;
+}
+
+}
+
