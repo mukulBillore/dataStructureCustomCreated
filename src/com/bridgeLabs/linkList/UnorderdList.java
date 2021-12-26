b549e9e07eed2412ee5397694c8ab8e7817d384a
mukulBillore <mukulbillore88@gmail.com>
2021-12-26 21:27:52 +0530

    added : added the unorderd list

---
+++ src/com/bridgeLabs/linkList/UnorderdList.java
@@ -0,0 +1,87 @@
+package com.bridgeLabs.linkList;
+
+public class UnorderdList {
+	//creating pointer/ref to the first node
+	OneNode head;
+	//search with particular element
+	public void searchElement(N e) {
+		OneNode temp=head;
+		boolean b = true;
+		while(temp.next!=null) {
+			if(temp.data==e) {
+				System.out.println("The data is found");
+				b=false;
+				break;
+			}
+		}if(b==true) {System.out.println("The data is not-found");}
+	}
+	//delete last node
+	public void popLast() {
+		if (head == null)
+		{}
+ 
+        if (head.next == null) {
+            head=null;
+        }
+ 
+        // Find the second last node
+        OneNode second_last = head;
+        while (second_last.next.next != null)
+            second_last = second_last.next;
+ 
+        // Change next of second last
+        second_last.next = null;
+ 
+     
+    }
+			
+			
+	// delete a first node 
+	public void pop() {
+		OneNode temp=head.next;
+		head=temp;
+	} 
+	//counting the size
+	public int size() {
+		int countSize=0;
+		OneNode let=head;
+		while(let.next!=null) {
+	        let.next=let;
+	        countSize++;
+		}
+		return countSize;
+	}
+	//add after certain node and count 
+	public void addANode(OneNode node,OneNode afterThisNode) {
+		OneNode temp = afterThisNode.next;
+		
+		int countSize=0;
+		if(temp==null) {
+			afterThisNode.next=node;
+		}
+		else {
+			afterThisNode.next=node;
+			node.next=temp;
+		}
+	
+	}
+	//add at last node
+	public void addANode(OneNode node ) {
+	//if the given node is the first element 
+		if(this.head==null) {
+		this.head=node;
+	}
+		//if given element is not the first element
+	else {
+		//temp var to store temperorly the head 
+		OneNode temp=head;
+		// till  node next is node we are accessing the last element  
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		temp.next=node;
+		}
+	 
+ }
+
+}
