c943a7eaca238bd0f03546378571f0912064dbeb
mukulBillore <mukulbillore88@gmail.com>
2021-12-26 18:27:31 +0530

    Added : uc3

--- src/com/bridgeLabs/linkList/MyMainClass.java
+++ src/com/bridgeLabs/linkList/MyMainClass.java
@@ -15,8 +15,8 @@ public class MyMainClass {
 		list1.addANode(node1);
 		list1.addANode(node2);
 		list1.addANode(node3);
-		//print.PrintLinklist(list1);
-		//uc2 is here
+		print.PrintLinklist(list1);
+		//uc3 is added
 		OneNode nodea = new OneNode(30);
 		OneNode nodeb = new OneNode(56);
 		OneNode nodec = new OneNode(70);
