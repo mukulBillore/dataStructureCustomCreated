e973982cc3d85c17a2a08c227a7109689caf0f23
mukulBillore <mukulbillore88@gmail.com>
2021-12-27 02:31:53 +0530

    added : added a certain bst in uc2

--- src/com/bridgelab/binarySearchTree/BinarySearchTree.java
+++ src/com/bridgelab/binarySearchTree/BinarySearchTree.java
@@ -8,7 +8,17 @@ public class BinarySearchTree {
 		utility.insert(56);
 		utility.insert(30);
 		utility.insert(70);
-
+		utility.insert(22);
+		utility.insert(40);
+		utility.insert(11);
+		utility.insert(3);
+		utility.insert(16);
+		utility.insert(70);
+		utility.insert(60);
+		utility.insert(65);
+		utility.insert(63);
+		utility.insert(67);
+		
 		utility.print();
 	}
 
